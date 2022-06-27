d57fe05c0c20e7a151f0a0e3bb83ee06305d01dd
Michael Friend <michael.l.friend2@gmail.com>
2022-06-27 10:27:10 -0500

    completed exercise

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-@SuppressWarnings("deprecation")
 public class Main {
     public static void main(String[] args) {
 
@@ -7,7 +6,7 @@ public class Main {
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+        for (String book: author.publishedBooks()) {
             System.out.println(book);
         }
     }
